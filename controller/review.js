a7b4a3554745af71625a4d87d460162607eba0cd
Sajan-Bhoyar123 <sajanbhoyar5@gmail.com>
2025-07-15 21:55:10 +0530

    Path change of review means redirect path

--- controller/review.js
+++ controller/review.js
@@ -11,21 +11,21 @@ const constructorReview =  async(req,res)=>{
      await newreview.save();
      await list.save();
      req.flash("success","successfully added your review");
-     res.redirect(`http://localhost:8080/listing/${id}`)
+     res.redirect(`/listing/${id}`);
 }
 
 const destructorReview = async(req,res)=>{
        let {id,reviewid} = req.params;
         await Listing.findByIdAndUpdate(id,{$pull:{reviews:reviewid}})
         await review.findByIdAndDelete(reviewid);
-          req.flash("success","successfully delete your review");
-        res.redirect(`http://localhost:8080/listing/${id}`)
+        req.flash("success","successfully delete your review");
+        res.redirect(`/listing/${id}`);
   }
 
 const RedirectRoute = (req,res)=>{
      let {id,reviewid} = req.params;
     req.flash("success","successfully login ");
-     res.redirect(`http://localhost:8080/listing/${id}`)
+     res.redirect(`/listing/${id}`);
 
 }
 module.exports = {constructorReview,destructorReview,RedirectRoute};
