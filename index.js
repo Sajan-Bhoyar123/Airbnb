1f4f041ef03d61d8f7142419e8204380e1a8214b
Sajan-Bhoyar123 <sajanbhoyar5@gmail.com>
2025-07-17 08:54:09 +0530

    Mobile friendly

--- index.js
+++ index.js
@@ -90,7 +90,7 @@ passport.deserializeUser(user.deserializeUser());
 app.use((req,res,next)=>{
      res.locals.success = req.flash("success");
      res.locals.error = req.flash("error")
-     res.locals.curruser = req.user;
+     res.locals.currUser = req.user;
      next();
 })
 
