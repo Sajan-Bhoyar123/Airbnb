3d4018a6e0e2a139acfcc88541f7169e1ed00c6a
Sajan-Bhoyar123 <sajanbhoyar5@gmail.com>
2025-07-17 09:18:10 +0530

    change file

--- index.js
+++ index.js
@@ -90,7 +90,7 @@ passport.deserializeUser(user.deserializeUser());
 app.use((req,res,next)=>{
      res.locals.success = req.flash("success");
      res.locals.error = req.flash("error")
-     res.locals.currUser = req.user;
+     res.locals.curruser = req.user;
      next();
 })
 
